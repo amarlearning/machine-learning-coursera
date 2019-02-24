546b207511a2a422a6e3bb0966b613376830c0b0
Amar Prakash Pandey <amar.om1994@gmail.com>
2019-02-24 23:28:14 +0530

    optimised code for gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,7 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:100000,
+for iter = 1:num_iters,
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -16,13 +16,9 @@ for iter = 1:100000,
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+
+    theta = theta .- (alpha .* (sum((X' * ((X * theta) - y)), 2) / (m)));
     
-    minTheta1 = sum((X * theta) - y) / (m);
-    minTheta2 = sum( X(:,2)' * ((X * theta) - y)) / (m);   
-
-    theta(1) = theta(1) - alpha * minTheta1;
-    theta(2) = theta(2) - alpha * minTheta2;
-
     % ============================================================
 
     % Save the cost J in every iteration    
